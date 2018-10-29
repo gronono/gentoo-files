d964154b9505ade37095371f9a9211f19e279464
gronono <arnaud.brunet+github@gmail.com>
2018-10-29 23:16:33 +1100

    shellcheck

--- home/arnaud/bin/change-recif-password.sh
+++ home/arnaud/bin/change-recif-password.sh
@@ -8,7 +8,7 @@ MOUNT_CREDENTIALS=/root/recif.passwd
 CNTLM_CONF=/etc/cntlm.conf
 CNTLM_PORT=3128
 PROXY=proxy-web.proxy-dmz.gnc:3128
-NO_PROXY=localhost,127.0.0.1,127.0.1.1,*.recif.nc,*.appli-gestion.nc,*.gnc,/var/run/docker.sock,10.10.106.0/24,*.valid-gouv.nc,*.gouv.nc,*.dmz.nc
+NO_PROXY="localhost,127.0.0.1,127.0.1.1,*.recif.nc,*.appli-gestion.nc,*.gnc,/var/run/docker.sock,10.10.106.0/24,*.valid-gouv.nc,*.gouv.nc,*.dmz.nc"
 
 
 if [ "$(id -u)" != "${ROOT_ID}" ]; then
@@ -17,11 +17,10 @@ if [ "$(id -u)" != "${ROOT_ID}" ]; then
 fi
 
 echo "Nouveau mot de passe Recif:"
-read -s password
+read -rs password
 
 echo "Démontage de ${PALO_ALTO_MOUNT}"
-umount ${PALO_ALTO_MOUNT}
-if [ $? -ne 0 ]; then
+if ! umount ${PALO_ALTO_MOUNT}; then
 	echo "Impossible de démonter ${PALO_ALTO_MOUNT}" 1>&2
 fi
 
@@ -33,14 +32,13 @@ EOF
 chmod 600 ${MOUNT_CREDENTIALS}
 
 echo "Remontage de ${PALO_ALTO_MOUNT}"
-mount ${PALO_ALTO_MOUNT}
-if [ $? -ne 0 ]; then
+if ! mount ${PALO_ALTO_MOUNT}; then
 	echo "Impossible de remonter ${PALO_ALTO_MOUNT}" 1>&2
 	exit 2
 fi
 
 echo "Génération du hash cntlm"
-hash=$(echo ${password} | cntlm -u ${RECIF_USER} -H | grep -v Password)
+hash=$(echo "${password}" | cntlm -u ${RECIF_USER} -H | grep -v Password)
 
 echo "Génération de ${CNTLM_CONF}"
 cat << EOF > ${CNTLM_CONF}
@@ -57,15 +55,13 @@ echo "Redemarrage du service cntlm"
 /etc/init.d/cntlm restart
 
 echo "Vérification PMU"
-curl -s http://pmu.fr | grep ${RECIF_USER} > /dev/null
-if [ $? -ne 0 ]; then
+if ! curl -s http://pmu.fr | grep -q ${RECIF_USER} > /dev/null; then
 	echo "Le proxy devrait affiché une page d'erreur contenant votre nom"
 	exit 3
 fi
 
 echo "Vérification JIRA"
-curl -s http://jira.gouv.nc > /dev/null
-if [ $? -ne 0 ]; then
+if ! curl -s http://jira.gouv.nc > /dev/null; then
 	echo "Vous n'avez pas accès à JIRA !"
 	exit 4
 fi
